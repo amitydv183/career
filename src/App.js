7a39a70552e5cc23f1340c12e3e601f729e699db
Amit Kumar <amkr9329@gmail.com>
2025-03-19 15:33:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+//import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import SkillsComponent from './pages/SkillsComponent';
+import JobMarketTrends from './pages/JobMarketTrends';
+import CareerAdvisor from './pages/CareerAdvisor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<SkillsComponent />} />
+          <Route path="/job" element={<JobMarketTrends/>} />
+          <Route path="/career-advisor" element={<CareerAdvisor />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
